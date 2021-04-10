3cf503feb3f50a5d7272aca609b9fd9b576fca7d
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-10 17:20:31 +0100

    Testing

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "gcs" {
-    bucket = "terraform-state-jf"   # GCS bucket name to store terraform tfstate
-    prefix = "prod"           # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
-    }
+    bucket = "terraform-state-jf"
+    prefix = "prod"
+  }
 }
 
 module "docker" {
