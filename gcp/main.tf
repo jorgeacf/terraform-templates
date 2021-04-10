9e357412471a8b2ee1f99af1be2a4c578db4d03c
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-10 16:36:20 +0100

    Added aditional bucket on GCP

--- gcp/main.tf
+++ gcp/main.tf
@@ -6,14 +6,14 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "gcs_bucket_0" {
-  name = "test-bucket-random-00000"
+  name = "test-bucket-random-000000"
 }
 
 resource "google_storage_bucket" "gcs_bucket_1" {
-  name = "test-bucket-random-11111"
+  name = "test-bucket-random-111111"
 }
 
 
 resource "google_storage_bucket" "gcs_bucket_2" {
-  name = "test-bucket-random-22222"
+  name = "test-bucket-random-222222"
 }
