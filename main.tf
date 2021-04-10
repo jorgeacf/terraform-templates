7cc79d3d9d6f87d55cd8bb74cbfd5c5b891c2e84
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-10 16:16:22 +0100

    Added modules for gcp and docker

--- main.tf
+++ main.tf
@@ -1,23 +1,8 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-    }
-  }
+
+module "docker" {
+  source = "./docker"
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+module "gcp" {
+  source = "./gcp"
 }
