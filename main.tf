8da870c15160874c0b49db79d5d19439c7af8b25
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-10 16:45:28 +0100

    Testing

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 
+
+data "terraform_remote_state" "terraform_remote_state_files" {
+  backend = "gcs"
+  config = {
+    bucket  = "terraform-state-jf"
+    prefix  = "prod"
+  }
+}
+
 module "docker" {
   source = "./docker"
 }
