45cd1d9c692a8e612d29f223c87d0e20fea48c27
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-12 15:57:08 +0100

    Added terraform cloud function module

---
+++ gcp/cloud-functions/main.tf
@@ -0,0 +1,39 @@
+resource "google_storage_bucket" "bucket" {
+  name = "test-bucket-jf"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "cloud-functions-java-release.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./cloud-functions-java-release.zip"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "java11"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  timeout               = 60
+  entry_point           = "helloGET"
+  labels = {
+    my-label = "my-label-value"
+  }
+
+  environment_variables = {
+    MY_ENV_VAR = "my-env-var-value"
+  }
+}
+
+# IAM entry for a single user to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "user:jorgeacf32@gmail.com"
+}
