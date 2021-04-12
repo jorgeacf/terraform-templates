dd511a7a7360828a1f4c7d4afddd5529965c4a2b
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-12 15:01:59 +0100

    Trying to download artefacts

--- gcp/main.tf
+++ gcp/main.tf
@@ -12,15 +12,3 @@ module "cloud-functions" {
 module "gcs" {
   source = "./gcs"
 }
-
-
-
-resource "null_resource" "cloud_functions_zip" {
-  triggers = {
-    on_version_change = "1"
-  }
-
-  provisioner "local-exec" {
-    command = "wget -o cloud-functions-java-release.zip https://github.com/jorgeacf/gcp-examples/releases/download/latest/cloud-functions-java-release.zip"
-  }
-}
