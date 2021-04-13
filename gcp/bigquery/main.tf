42d77449cc6d1a7cd1ddfe46d787e11d709117c8
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-13 14:32:39 +0100

    Added BigQuery submodules

--- gcp/bigquery/main.tf
+++ gcp/bigquery/main.tf
@@ -9,29 +9,6 @@ resource "google_project_service" "api_iam" {
   service = "iam.googleapis.com"
 }
 
-
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "example_dataset"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "EU"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-
-  access {
-    role          = "OWNER"
-    user_by_email = google_service_account.bqowner.email
-  }
-
-  access {
-    role   = "READER"
-    domain = "hashicorp.com"
-  }
-}
-
-resource "google_service_account" "bqowner" {
-  account_id = "bqowner"
+module "dataset" {
+  source = "./dataset"
 }
