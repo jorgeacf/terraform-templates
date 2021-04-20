3ffa992cd6277bc6cfbfdd6dba2d21f91d203847
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2021-04-20 13:21:42 +0100

    Added new bucket for tests

--- gcp/gcs/main.tf
+++ gcp/gcs/main.tf
@@ -7,7 +7,10 @@ resource "google_storage_bucket" "gcs_bucket_1" {
   name = "test-bucket-random-111111"
 }
 
-
 resource "google_storage_bucket" "gcs_bucket_2" {
+  name = "test-bucket-random-222222"
+}
+
+resource "google_storage_bucket" "gcs_bucket_3" {
   name = "test-bucket-random-222222"
 }
